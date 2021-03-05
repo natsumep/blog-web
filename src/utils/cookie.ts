820a464053d3bf5e84c7610e1c3a99ddc7446c81
p.p <554096147@qq.com>
2021-03-05 10:41:35 +0800

    fix(cookie): fix server get cookie

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -1,6 +1,6 @@
 export function getCookie(cookie: string, cname: string) {
   const name = cname + '='
-  const ca = cookie.split(';')
+  const ca = cookie && cookie.split && cookie.split(';')
   for (let i = 0; i < ca.length; i++) {
     const c = ca[i].trim()
     if (c.indexOf(name) === 0) return c.substring(name.length, c.length)
