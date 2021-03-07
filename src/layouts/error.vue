b6969c35f8bcb1b0604344345eefeea5b9e19bf8
pp <554096147@qq.com>
2021-03-07 16:21:11 +0800

    fix(error): fix 修改错误页代码

--- src/layouts/error.vue
+++ src/layouts/error.vue
@@ -1,16 +1,21 @@
 <template>
   <div style="height: 100vh" class="flex-center">
-    <section class="error-container">
+    <section v-if="error.statusCode === 404" class="error-container">
       <span class="four"><span class="screen-reader-text">4</span></span>
       <span class="zero"><span class="screen-reader-text">0</span></span>
       <span class="four"><span class="screen-reader-text">4</span></span>
     </section>
+    <div v-else>
+      {{ error }}
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 export default {
   components: {},
+  // eslint-disable-next-line vue/require-prop-types
+  props: ['error'],
   data() {
     return {}
   },
