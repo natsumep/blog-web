4a0c57e1e1350842544f819b8366606020b8d7e8
p.ppppppppppppp <554096147@qq.com>
2021-03-09 01:15:08 +0800

    fix(ba): try fix bug

--- src/plugins/ba.client.ts
+++ src/plugins/ba.client.ts
@@ -4,7 +4,7 @@
  */
 if (process.client && process.env.NODE_ENV === 'production') {
   // eslint-disable-next-line no-unused-vars
-  const _hmt = (window as any)._hmt || []
+  // const _hmt = (window as any)._hmt || []
   ;(function () {
     const hm = document.createElement('script')
     hm.src = 'https://hm.baidu.com/hm.js?33ee44d7f5f6d433675eda12e658fdc8'
