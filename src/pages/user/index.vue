c455e330366eaaca0ab5bf429e27868cba128b79
p.p <554096147@qq.com>
2021-03-09 14:44:22 +0800

    fix(user): 修复确认密码判断异常bug

--- src/pages/user/index.vue
+++ src/pages/user/index.vue
@@ -192,7 +192,7 @@ export default {
     validatePass2(rule, value, callback) {
       if (value === '') {
         callback(new Error('请再次输入密码'))
-      } else if (value !== this.ruleForm.pass) {
+      } else if (value !== this.ruleForm.password) {
         callback(new Error('两次输入密码不一致!'))
       } else {
         callback()
