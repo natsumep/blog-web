16280d290479d5596d07666dfdd1943696af2eec
p.p <554096147@qq.com>
2021-03-09 17:59:51 +0800

    chore: 修改端口号

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,10 +3,10 @@
 import { SERVER_PATH, SERVER_USER_PATH } from './src/config'
 export default {
   srcDir: 'src/',
-  server: {
-    port: 8000, // default: 3000
-    host: '0.0.0.0', // default: localhost
-  },
+  // server: {
+  //   port: 8000, // default: 3000
+  //   host: '0.0.0.0', // default: localhost
+  // },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'blog-web',
