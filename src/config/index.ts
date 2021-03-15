3d57c97c65ae987f56ac3b7b1f647ee1992550bf
p.p <554096147@qq.com>
2021-03-15 11:48:04 +0800

    fix: 修改服务器地址

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
-// export const SERVER_PATH = 'https://www.tinker.run/api/'
-// export const SERVER_USER_PATH = 'https://www.tinker.run/api/' // 'http://127.0.0.1:8088/api/'
-export const SERVER_PATH = 'http://127.0.0.1:8088/api/'
-export const SERVER_USER_PATH = 'http://127.0.0.1:8088/api/'
+export const SERVER_PATH = 'https://www.tinker.run/api/'
+export const SERVER_USER_PATH = 'https://www.tinker.run/api/' // 'http://127.0.0.1:8088/api/'
+// export const SERVER_PATH = 'http://127.0.0.1:8088/api/'
+// export const SERVER_USER_PATH = 'http://127.0.0.1:8088/api/'
 
 // axios 默认配置
 export const AXIOS_DEFAULT_CONFIG = {
