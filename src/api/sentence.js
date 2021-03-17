192e9ad1955a47cfd1663b6034e2191fdd02a63d
p.p <554096147@qq.com>
2021-03-17 17:08:21 +0800

    fix: change api

--- src/api/sentence.js
+++ src/api/sentence.js
@@ -4,20 +4,20 @@ export const sentence = [
     method: 'GET',
     path: '/',
     notShowError: true,
-    serverPath: 'https://api.tinker.run/oword',
+    serverPath: 'https://api.tinker.run/sentence',
   },
   {
     name: 'like',
     method: 'POST',
     path: '/like/:id',
     notShowError: true,
-    serverPath: 'https://api.tinker.run/oword',
+    serverPath: 'https://api.tinker.run/sentence',
   },
   {
     name: 'unlike',
     method: 'POST',
     path: '/unlike/:id',
     notShowError: true,
-    serverPath: 'https://api.tinker.run/oword',
+    serverPath: 'https://api.tinker.run/sentence',
   },
 ]
