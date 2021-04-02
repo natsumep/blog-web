0659775b2c9143aef59db21608e03376e4aac26e
p.p <554096147@qq.com>
2021-04-02 15:30:06 +0800

    feat: 增加api文档

---
+++ src/pages/api/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="api-content">
+    <iframe
+      width="100%"
+      height="100%"
+      src="https://api.tinker.run/docs"
+      frameborder="0"
+    ></iframe>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+@Component
+export default class Index extends Vue {}
+</script>
+<style lang="less" scoped>
+.api-content {
+  width: 100%;
+  height: calc(100vh - 61px);
+}
+</style>
