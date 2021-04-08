a079fed462ca05f8c27ec05be55ee61c59e74eed
p.p <554096147@qq.com>
2021-04-08 15:00:31 +0800

    fix: fix undfined

--- src/plugins/visit-view.client.ts
+++ src/plugins/visit-view.client.ts
@@ -2,7 +2,7 @@ import { getUrlParam } from '~/utils/browser'
 export default function (context: any) {
   // Add the userAgent property to the context
   if (process.env.NODE_ENV === 'production') {
-    const formType = getUrlParam('f')
+    const formType = getUrlParam('f') || ''
     const userAgent = process.server
       ? context.req.headers['user-agent']
       : navigator.userAgent
