99b94a45f0c5795d618d835ed2eb52c606d57474
p.p <554096147@qq.com>
2021-05-08 09:40:33 +0800

    fix: 修改文件上传 支持大文件压缩上传

--- src/utils/uploadFile.ts
+++ src/utils/uploadFile.ts
@@ -1,15 +1,37 @@
 import { $axios } from '@/utils/api'
-import { Message } from 'element-ui'
+// import { Message } from 'element-ui'
+import Compressor from 'compressorjs'
 export async function updateFile(blob: Blob) {
-  if (blob.size > 300 * 1024) {
-    Message.error('图片超过最大尺寸300kb')
-    return false
+  const size = blob.size
+  let file: any = blob
+  if (size > 1024 * 950) {
+    try {
+      file = await compressorFile(blob)
+    } catch (e) {
+      return false
+    }
   }
   const formDate = new FormData()
-  formDate.append('file', blob)
+  formDate.append('file', file, file.name)
   formDate.append('time', String(+new Date()))
   const data: any = await $axios.post('/upload', formDate).catch()
   // callback(url)
   const name = (data.url && data.url[0]) || ''
   return name
+}
+
+function compressorFile(blob: Blob): any {
+  return new Promise((resolve, reject) => {
+    // eslint-disable-next-line no-new
+    new Compressor(blob, {
+      quality: 1,
+      maxWidth: 1000,
+      success(result) {
+        resolve(result)
+      },
+      error(err) {
+        reject(err)
+      },
+    })
+  })
 }
